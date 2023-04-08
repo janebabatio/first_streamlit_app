e603297e4e91fdcada960ac5da78ee524b75d32d
janebabatio <129716132+janebabatio@users.noreply.github.com>
2023-04-08 11:28:07 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My parents New Healthy Diner)
+streamlit.title('My parents New Healthy Diner')
 
                 streamlit.header('Breakfast Menu')
                 
